b19b6f33a892f3889f97b182a76f21886fb4f3eb
nayan-ray <niranzanroy1@gmail.com>
2024-08-31 11:24:00 +0600

    draw

--- BLOG-WEVSITE/src/pages/social/SocialHome.jsx
+++ BLOG-WEVSITE/src/pages/social/SocialHome.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./socialHome.css"
+import RssFeedIcon from '@mui/icons-material/RssFeed';
 
 const SocialHome = () => {
   return (
     <div className='social-home mt-5'>
         <div className="container">
             <div className="row">
-                <div className="col-6 bg-secondary">
-                     socail
+                <div className="col-6 bg-secondary fs-2">
+                     <RssFeedIcon />
                 </div>
                 
             </div>
